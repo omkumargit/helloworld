9cac95dd2b0745f4ea7980af81a45d8e4b5456b8
omkumargit <omdsiml@gmail.com>
2016-11-29 20:36:47 -0800

    Add files via upload
    in my code i have added a method m1

--- Arr.java
+++ Arr.java
@@ -4,12 +4,17 @@ public class Arr {
 	/**
 	 * @param args
 	 */
+	 public static void m1(){
+		 System.out.println("helloindia");
+	 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 byte []a=new byte[1];long[]b=new long[1];
 float[]c=new float[1];Object[]d=new Object[1];
 System.out.println(a[0]+","+b[0]+","+c[0]+","+d[0]);
 int a=90;
+Arr.m1();
+
 	}
 
 }
