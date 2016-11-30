682af89e380ee4ace940a59ff1696278f7dfc3e7
omkumargit <omdsiml@gmail.com>
2016-11-30 09:46:22 +0530

    Add files via upload

---
+++ Arr.java
@@ -0,0 +1,14 @@
+
+public class Arr {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+byte []a=new byte[1];long[]b=new long[1];
+float[]c=new float[1];Object[]d=new Object[1];
+System.out.println(a[0]+","+b[0]+","+c[0]+","+d[0]);
+	}
+
+}
