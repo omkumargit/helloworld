1bd89a8c21c8716c0289eb8f20831fbced7e3c11
omkumargit <omdsiml@gmail.com>
2016-11-30 09:51:17 +0530

    Add files via upload
    updated for inhancement

--- Arr.java
+++ Arr.java
@@ -9,6 +9,7 @@ public class Arr {
 byte []a=new byte[1];long[]b=new long[1];
 float[]c=new float[1];Object[]d=new Object[1];
 System.out.println(a[0]+","+b[0]+","+c[0]+","+d[0]);
+int a=90;
 	}
 
 }
